7728609a412817b9ed5bbe0295c95c8a34f5f24c
Kolby Tishler <kbtishler@crimson.ua.edu>
2021-09-07 17:53:17 -0500

    project commit

--- Program.cs
+++ Program.cs
@@ -6,7 +6,15 @@ namespace exapmlerepo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter views:");
+            int views = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter ad rate:");
+            double adRate = double.Parse(Console.ReadLine());
+            adRate = adRate / 100;
+            double adViews = views * adRate;
+            double revenue = (.18 * adViews) * .68;
+            Console.WriteLine("Your total revenue is: " + revenue);
+            Console.ReadLine();
         }
     }
 }
